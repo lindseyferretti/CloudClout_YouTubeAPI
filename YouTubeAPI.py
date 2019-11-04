c0899189c6e9e14e80d2b091f58496898eefe5c3
Catherine Osadciw <ceo2869@rit.edu>
2019-11-04 05:21:37 -0500

    Reworked script to accept guide category ID as arg (run 1 at a time, a few a day)

--- YouTubeAPI.py
+++ YouTubeAPI.py
@@ -9,99 +9,17 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import sys
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    request = youtube.guideCategories().list(
-        part="snippet",
-        regionCode="US"
-    )
-    response = request.execute()
-
-    print(response)
-
-    guide_categories = {} # maps guide category IDs to names
-    channels = {} # maps guide category IDs to a list of channel IDs
-    playlists = {} # maps channel IDs to uploaded playlist IDs
-    videos = {} # maps playlist IDs to a list of uploaded videos
-    tags = {} # maps video ID to a list of tags
-
-    # Iterate through result, pulling out guideCategories (high-level channel
-    # classifications)
-    results = response["items"]
-    for result in results:
-        category_id = result["snippet"]["channelId"]
-        category_name = result["snippet"]["title"]
-        guide_categories[category_id] = category_name
-
-    # For each guide category, pull a list of channels associated with that
-    # guide category
-    categories = guide_categories.keys()
-    for category in categories:
-        # Request channels with categoryId = category
-        response = run_channel_request(youtube, None, category)
-        next_page_token = "" # Just make something non-None so loops runs once
-
-        # Create list of channel IDs from response
-        channel_list = []
-        while next_page_token is not None:
-            results = response["items"]
-            for result in results:
-                channel_id = result["id"]
-                channel_list.append(channel_id)
-
-                # While we're on a channel ID, we'll pull its uploaded
-                # videos playlist ID from the contentDetails
-                playlist_id = result["contentDetails"]["relatedPlaylists"]["uploads"]
-                playlists[channel_id] = playlist_id
-
-                # Next, we'll request all the videos in the playlist
-                uploads_response = run_playlist_items_request(youtube,
-                                                              None,
-                                                              playlist_id)
-                uploads_next_page_token = ""
-
-                # Create list of videos from response
-                videos_list = []
-                while uploads_next_page_token is not None:
-                    video_results = uploads_response["items"]
-                    for video_result in video_results:
-                        video_id = video_result["contentDetails"]["videoId"]
-                        videos_list.append(video_id)
-
-                        # Last step: pull all the tags for each video
-                        tags_response = run_tags_request(youtube, None,
-                                                         video_id)
-
-                        # Create list of tags from response (top 50 tags)
-                        tags_list = tags_response["items"]["snippet"]["tags"]
-                        tags[video_id] = tags_list
-
-                    uploads_next_page_token = get_next_page_token(uploads_response)
-                    uploads_response = run_playlist_items_request(youtube,
-                                                                  uploads_next_page_token,
-                                                                  playlist_id)
-                videos[playlist_id] = videos_list
-            next_page_token = get_next_page_token(response)
-            response = run_channel_request(youtube, next_page_token, category)
-        # Add list of channels for category to channels map
-        channels[category] = channel_list
+    category_id = sys.argv[1]
+    print(category_id)
+    channels_to_tags = get_data(category_id)
+    print(channels_to_tags)
+    load_data(channels_to_tags)
 
 
 def run_channel_request(youtube, next_page_token, category):
@@ -127,13 +45,13 @@ def run_playlist_items_request(youtube, next_page_token, playlist_id):
         request = youtube.playlistItems().list(
             part="contentDetails",
             playlistId=playlist_id,
-            maxResults=50
+            maxResults=25
         )
     else:
         request = youtube.playlistItems().list(
             part="contentDetails",
             playlistId=playlist_id,
-            maxResults=50,
+            maxResults=25,
             pageToken=next_page_token
         )
     response = request.execute()
@@ -164,6 +82,108 @@ def get_next_page_token(response):
     except:
         next_page_token = None
     return next_page_token
+
+
+def aggregate_tags(tags):
+    aggregate_tags = {}
+    for channel in tags:
+        tag_dict = tags[channel]
+        max_tags = []
+        index = 0
+        while index < 10 and len(tag_dict) > 0:
+            max_tag = max(tag_dict, key=lambda k: tag_dict[k])
+            max_tags.append(max_tag)
+            tag_dict.pop(max_tag)
+            index += 1
+        aggregate_tags[channel] = max_tags
+    return aggregate_tags
+
+
+def get_data(category):
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "secrets.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    channels = {}  # maps guide category IDs to a list of channel IDs
+    playlists = {}  # maps channel IDs to uploaded playlist IDs
+    videos = {}  # maps playlist IDs to a list of uploaded videos
+    tags = {}  # maps channel ID to dict of tags => num of occurrences
+
+    # For guide category, pull a list of channels associated with that
+    # guide category
+    # Request channels with categoryId = category
+    response = run_channel_request(youtube, None, category)
+
+    # Create list of channel IDs from response
+    # Only taking top 50 channels returned
+    channel_list = []
+    results = response["items"]
+    for result in results:
+        channel_id = result["id"]
+        channel_list.append(channel_id)
+
+        # While we're on a channel ID, we'll pull its uploaded
+        # videos playlist ID from the contentDetails
+        playlist_id = result["contentDetails"]["relatedPlaylists"][
+            "uploads"]
+        playlists[channel_id] = playlist_id
+
+        # Next, we'll request all the videos in the playlist
+        uploads_response = run_playlist_items_request(youtube, None,
+                                                      playlist_id)
+
+        # Create list of videos from response
+        # Only going to use the 25 most recent uploads (no pagination)
+        videos_list = []
+        video_results = uploads_response["items"]
+        for video_result in video_results:
+            video_id = video_result["contentDetails"]["videoId"]
+            videos_list.append(video_id)
+
+            # Last step: pull all the tags for each video
+            tags_response = run_tags_request(youtube, None, video_id)
+            snippet = tags_response["items"][0]["snippet"]
+
+            # Create list of tags from response (top 50 tags)
+            # Note that every now and then a video has no tags
+            if channel_id in tags:
+                tag_dict = tags[channel_id]
+            else:
+                tag_dict = {}
+                tags[channel_id] = tag_dict
+            try:
+                tags_list = snippet["tags"]
+                for tag in tags_list:
+                    if tag in tag_dict:
+                        tag_dict[tag] += 1
+                    else:
+                        tag_dict[tag] = 1
+
+            except KeyError:
+                print("No tags for this video")
+        videos[playlist_id] = videos_list
+    # Add list of channels for category to channels map
+    channels[category] = channel_list
+
+    # Aggregate tags per channel ID into lists of top 10 tags
+    aggregated_tags = aggregate_tags(tags)
+    return aggregated_tags
+
+
+def load_data(channels_to_tags):
+    # TODO: create files in CSV format for Gremlin, dump in S3 bucket
+    pass
 
 
 if __name__ == "__main__":
