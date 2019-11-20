0e29eec2e64c968af69b913d558bb0922fc03f7d
txl3828 <txl3828@g.rit.edu>
2019-11-20 17:13:29 -0500

    Data loader implementation started. Verticies being written

--- DataLoader.py
+++ DataLoader.py
@@ -12,14 +12,22 @@ def main():
     json_file.close()
     channels_to_tags = json.loads(json_text)
 
-    print(channels_to_tags)
     load_data(channels_to_tags)
 
 
 def load_data(channels_to_tags):
-    # TODO: create files in CSV format for Gremlin, dump in S3 bucket
-    pass
-
+    # TODO: create files in CSV format for Gremlin, dump in S3 bucket 
+    i = 1
+    write = open("verticies.csv", "a")
+    write.write("~id, videoID:string, tags:string[]\n")
+    for key in channels_to_tags:
+        string_to_write = str(i) + ", " + key + ", \""
+        for tag in channels_to_tags[key]:
+            string_to_write += tag + "; "
+        string_to_write += "\"\n"
+        write.write(string_to_write)
+        i += 1
+    write.close()
 
 if __name__ == "__main__":
     main()
